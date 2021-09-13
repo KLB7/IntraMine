701481428a1e1ea65e50d48d1abdf4c6b4cd59a8
KLB7 <klb7@intramine.info>
2021-09-13 16:21:33 -0400

    Update autoLinks.js
    Autolinking was slow in large Perl and text files. Due to perhaps the dumbest bug I've ever committed.
    Affected: autoLinks.js#24.

--- js_for_web_server/autoLinks.js
+++ js_for_web_server/autoLinks.js
@@ -21,7 +21,10 @@ function addAutoLinks() {
 	let lastVisibleLineNum = lastVisibleLineNumber(el);
 	// Go past the window bottom, sometimes linkage removes so much text
 	// that fresh lines come into view. And this makes scrolling smoother.
-	lastVisibleLineNum = Math.floor(lastVisibleLineNum * 2.1);
+	lastVisibleLineNum = Math.floor(firstVisibleLineNum + (lastVisibleLineNum - firstVisibleLineNum) * 1.5);
+	
+	// I've left this line below as an exemplar of how stupid I can be.
+	//lastVisibleLineNum = Math.floor(lastVisibleLineNum * 2.1);
 
 	let rowIds = []; // track <tr id='rowId' fo reach line, in sequence.
 	getVisibleRowIds(firstVisibleLineNum, lastVisibleLineNum, rowIds);
